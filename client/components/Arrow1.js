eb32bf02be99958bd9656eb51dd0dcbe67fd3385
Krzysztof Szypulski <krzychu700@gmail.com>
2018-11-28 20:57:22 +0100

    task 1 finished

---
+++ client/components/Arrow1.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const
+Arrow1 = props => (
+return ();
+);
+
+export default
+Arrow1;
---
+++ client/components/Arrow2/Arrow2.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const
+Arrow2 = props => (
+return ();
+);
+
+export default
+Arrow2;
